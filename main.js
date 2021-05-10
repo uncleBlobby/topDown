355d687fb46fa35c1c58aa8dc2e4224042204f62
uncleBlobby <dc.christianson@gmail.com>
2021-05-09 19:49:08 -0600

    made it canvas intead of divs

--- main.js
+++ main.js
@@ -1,62 +1,112 @@
-//function to draw spritegrid on background
+//draw grid on canvas
 
-const gameWindow = document.getElementById("gameWindow");
+let gameCanvas = document.getElementById("gameCanvas");
+let ctx = gameCanvas.getContext("2d");
 
-// 16 x 16 grid of 64px x 64px squares
+let grassSprite = new Image(64, 64);
+grassSprite.src = "./sprites/simpleGrass.png";
+
+let wallSprite = new Image(64, 64);
+wallSprite.src = "./sprites/stoneWall.png";
+
+let playerSprite = new Image(64, 64);
+playerSprite.src = "./sprites/player/playerChar.png";
 
 
-function drawBGSpriteGrid(){
-    for(let i = 0; i < 256; i++){
-        let bgSpriteChunk = document.createElement("div");
-        bgSpriteChunk.id = "bgSprite-" + i;
-        bgSpriteChunk.classList.add("bgSpriteChunk");
-        gameWindow.appendChild(bgSpriteChunk);
-    }
+function loadGrassSprite(){
+
+let grassSprite = new Image();
+grassSprite.src = "./sprites/simpleGrass.png";
+ctx.drawImage(grassSprite, 0, 0, 64, 64);
+console.log(grassSprite);
+console.log(grassSprite.src);
 }
 
-drawBGSpriteGrid();
 
-//function to move player sprite
 
-const playerChar = document.getElementById("playerChar");
-let playerPosX = 512;
-let playerPosY = 512;
+function drawLineX(x){
+    ctx.beginPath();
+    ctx.moveTo(x,0);
+    ctx.lineTo(x, 1024);
+    ctx.stroke();
+}
+
+function drawLineY(y){
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(1024, y);
+    ctx.stroke();
+}
+
+function drawSpriteGrid(){
+for (i = 0; i < 1024; i += 64){
+    for (j = 0; j < 1024; j += 64){
+    ctx.drawImage(grassSprite, i, j, 64, 64);
+    }
+    //drawLineX(i);
+    //drawLineY(i);
+}
+}
+
+let playerX = 512;
+let playerY = 512;
+
+function drawPlayerSprite(){
+    //ctx.clearRect(playerX, playerY, 64, 64);
+    ctx.drawImage(playerSprite, playerX, playerY, 64, 64);
+}
+//ctx.drawImage(grassSprite, 0, 0, 64, 64);
+
+grassSprite.onload = function() {
+    drawSpriteGrid();
+};
+
+playerSprite.onload = function () {
+    drawPlayerSprite();
+    const playerChar = document.getElementById("playerSprite");
+};
 
 window.addEventListener("keydown", e => {
     if(e.code == "ArrowLeft"){
-
-        playerChar.style.left = playerPosX + "px";
-        console.log("move left");
-        playerChar.style.transform = "translateX(-64px)";
-        playerChar.style.left = (playerPosX - 64) + "px";
-        playerPosX = playerPosX - 64;
-        console.log(playerPosX + ", " + playerPosY);
+       drawSpriteGrid();
+       playerSprite.style.left = playerX + "px";
+       console.log("moved left");
+       playerSprite.style.transform = "translateX(-64px)";
+       playerSprite.style.left = (playerX - 64) + "px";
+       playerX = playerX - 64;
+       console.log(playerX + ", " + playerY);
+       drawPlayerSprite();
     }
     if(e.code == "ArrowRight"){
-        playerChar.style.left = playerPosX + "px";
-        console.log("move right");
-        playerChar.style.transform = "translateX(64px)";
-        playerChar.style.left = (playerPosX + 64) + "px";
-        playerPosX = playerPosX + 64;
-        console.log(playerPosX + ", " + playerPosY);
+        drawSpriteGrid();
+        playerSprite.style.left = playerX + "px";
+        console.log("moved right");
+        playerSprite.style.transform = "translateX(64px)";
+        playerSprite.style.left = (playerX + 64) + "px";
+        playerX = playerX + 64;
+        console.log(playerX + ", " + playerY);
+        drawPlayerSprite();
     }
     if(e.code == "ArrowUp"){
-        playerChar.style.top = playerPosY + "px";
-        console.log("move up");
-        playerChar.style.transform = "translateY(-64px)";
-        playerChar.style.top = (playerPosY - 64) + "px";
-        playerPosY = playerPosY - 64;
-        console.log(playerPosX + ", " + playerPosY);
-    }
-    if(e.code == "ArrowDown"){
-        playerChar.style.top = playerPosY + "px";
-        console.log("move down");
-        playerChar.style.transform = "translateY(64px)";
-        playerChar.style.top = (playerPosY + 64) + "px";
-        playerPosY = playerPosY + 64;
-        console.log(playerPosX + ", " + playerPosY);
-    }
-    //console.log(e);
+        drawSpriteGrid();
+        playerSprite.style.top = playerY + "px";
+        console.log("moved up");
+        playerSprite.style.transform = "translateY(-64px)";
+        playerSprite.style.top = (playerY - 64) + "px";
+        playerY = playerY - 64;
+        console.log(playerX + ", " + playerY);
+        drawPlayerSprite();
+     }
+     if(e.code == "ArrowDown"){
+         drawSpriteGrid();
+         playerSprite.style.top = playerY + "px";
+         console.log("moved up");
+         playerSprite.style.transform = "translateY(64px)";
+         playerSprite.style.top = (playerY + 64) + "px";
+         playerY = playerY + 64;
+         console.log(playerX + ", " + playerY);
+         drawPlayerSprite();
+     }        
 });
 
 
